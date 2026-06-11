e8ce62964023fe1045acb3d979c41223c1c5d32d
RocketRaider76 <jerrett@learnimrsv.com>
2026-06-11 15:46:01 -0600

    Delete pmsd-site directory

--- pmsd-site/netlify/functions/load-config.js
+++
@@ -1,9 +0,0 @@
-const { getStore } = require("@netlify/blobs");
-exports.handler = async function(event) {
-  const headers = {"Content-Type":"application/json","Access-Control-Allow-Origin":"*"};
-  try {
-    const store = getStore({ name: "lnm-platform", siteID: process.env.NETLIFY_SITE_ID, token: process.env.NETLIFY_AUTH_TOKEN });
-    const config = await store.get("config", { type: "json" });
-    return { statusCode: 200, headers, body: JSON.stringify({ ok: true, config: config || null }) };
-  } catch(err) { return { statusCode: 200, headers, body: JSON.stringify({ ok: true, config: null }) }; }
-};
